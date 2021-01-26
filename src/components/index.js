c5b262fe41983ae98d6f27498c492a991dabf9cf
Steve Calverley <steve.calverley@heffron.com.au>
2021-01-26 12:23:36 +1100

    [CM-18] Select - add m select - add m select option

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@ import { BadgePlugin } from './MBadge';
 import { ButtonPlugin } from './MButton';
 import { IconPlugin } from './MIcon';
 import { InputPlugin } from './MInput';
+import { InputGroupPlugin } from './MInputGroup';
+import { SelectPlugin } from './MSelect';
 
 // Main plugin to install all component group plugins
 export const componentsPlugin = /*#__PURE__*/ pluginFactory({
@@ -12,5 +14,7 @@ export const componentsPlugin = /*#__PURE__*/ pluginFactory({
     ButtonPlugin,
     IconPlugin,
     InputPlugin,
+    InputGroupPlugin,
+    SelectPlugin,
   },
 });
